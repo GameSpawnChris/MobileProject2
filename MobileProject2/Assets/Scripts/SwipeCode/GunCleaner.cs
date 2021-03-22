8eb2431860b195e9c43a482ae7da8d7a9d0f2eda
Chris <hickmanc032@gmail.com>
2021-03-21 20:14:07 -0400

    Gun Cleaning Test

---
+++ MobileProject2/Assets/Scripts/SwipeCode/GunCleaner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunCleaner : MonoBehaviour
+{
+    public GameObject mask;
+    bool pressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //pos.z = -6;
+
+        if(pressed==true)
+        {
+            GameObject ob = Instantiate(mask, pos, Quaternion.identity);
+            ob.transform.parent = GameObject.Find("GunCleaner").transform;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            pressed = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            pressed = false;
+        }
+    }
+}
