989f3163fd39aa32144cfa073ee03e9fc4cc3af8
Chris <hickmanc032@gmail.com>
2021-04-02 09:52:40 -0400

    Shooting mini-game prototype

---
+++ MobileProject2/Assets/Scripts/ClickToShoot/PointClick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointClick : MonoBehaviour
+{
+    public GameObject crossHairs;
+    private Vector3 target;
+
+
+    private void Update()
+    {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crossHairs.transform.position = target; //new Vector2(target.x, target.y);
+
+        if (Input.GetMouseButtonUp(0))
+        {
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    PrintName(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    private void PrintName (GameObject gameObject)
+    {
+        print(gameObject.name);
+    }
+
+}
