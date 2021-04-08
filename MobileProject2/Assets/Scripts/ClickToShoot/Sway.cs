c830f2c132fa6b8d91e383650d494d450c94a7bb
Chris <hickmanc032@gmail.com>
2021-04-07 20:56:49 -0400

    Fixed View

--- MobileProject2/Assets/Scripts/ClickToShoot/Sway.cs
+++ MobileProject2/Assets/Scripts/ClickToShoot/Sway.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Sway : MonoBehaviour
 {
-    public Vector3 pointA = new Vector3(0, 0, 0);
-    public Vector3 pointB = new Vector3(1, 1, 1);
+    public Vector3 pointA;
+    public Vector3 pointB;
     void Update()
     {
         transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
