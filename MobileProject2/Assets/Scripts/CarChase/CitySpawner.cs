f28671d0494aa20b9d17988256d5820e1ffa078b
Chris <Hickmanc032@gmail.com>
2021-04-29 18:21:29 -0400

    Car Chase Scene

---
+++ MobileProject2/Assets/Scripts/CarChase/CitySpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitySpawner : MonoBehaviour
+{
+    public GameObject CityBlockPrefab1;
+    public GameObject CityBlockPrefab2;
+    public GameObject CityBlockPrefab3;
+    public bool CarScene;
+    public float blockDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnCityBlock()); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnCityBlock()
+    {
+        int cityBlockType;
+
+        while (CarScene == true)
+        {
+            while (true)
+            {
+                cityBlockType = Random.Range(1, 4);
+                Debug.Log(cityBlockType);
+
+                if (cityBlockType == 1)
+                {
+                    Vector3 spawnPos = new Vector3(20f, 0f);
+                    Instantiate(CityBlockPrefab1, spawnPos, Quaternion.identity);
+                    yield return new WaitForSeconds(blockDelay);
+                }
+                if (cityBlockType == 2)
+                {
+                    Vector3 spawnPos = new Vector3(20f, 0f);
+                    Instantiate(CityBlockPrefab2, spawnPos, Quaternion.identity);
+                    yield return new WaitForSeconds(blockDelay);
+                }
+                if (cityBlockType == 3)
+                {
+                    Vector3 spawnPos = new Vector3(20f, 0f);
+                    Instantiate(CityBlockPrefab3, spawnPos, Quaternion.identity);
+                    yield return new WaitForSeconds(blockDelay);
+                }
+            }
+        }
+    }
+}
