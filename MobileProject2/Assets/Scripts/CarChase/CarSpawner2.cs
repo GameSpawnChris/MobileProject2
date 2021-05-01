1b5b462559c6b288256189f22be0ffbace610b2e
Chris <Hickmanc032@gmail.com>
2021-05-01 14:44:19 -0400

    Car Chase Scene

--- MobileProject2/Assets/Scripts/CarChase/CarSpawner2.cs
+++ MobileProject2/Assets/Scripts/CarChase/CarSpawner2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CitySpawner : MonoBehaviour
+public class CarSpawner2 : MonoBehaviour
 {
     public GameObject CityBlockPrefab1;
     public GameObject CityBlockPrefab2;
@@ -14,41 +14,41 @@ public class CitySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnCityBlock()); 
+        StartCoroutine(SpawnCityBlock());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     IEnumerator SpawnCityBlock()
     {
-        int cityBlockType;
+        int cityBlockType2;
 
         while (CarScene == true)
         {
             while (true)
             {
-                cityBlockType = Random.Range(1, 4);
-                Debug.Log(cityBlockType);
+                cityBlockType2 = Random.Range(1, 4);
+                Debug.Log(cityBlockType2);
 
-                if (cityBlockType == 1)
+                if (cityBlockType2 == 1)
                 {
-                    Vector3 spawnPos = new Vector3(startPoint, 0f);
+                    Vector3 spawnPos = new Vector3(startPoint, 0f, -30f);
                     Instantiate(CityBlockPrefab1, spawnPos, Quaternion.identity);
                     yield return new WaitForSeconds(blockDelay);
                 }
-                if (cityBlockType == 2)
+                if (cityBlockType2 == 2)
                 {
-                    Vector3 spawnPos = new Vector3(startPoint, 0f);
+                    Vector3 spawnPos = new Vector3(startPoint, 0f, -30f);
                     Instantiate(CityBlockPrefab2, spawnPos, Quaternion.identity);
                     yield return new WaitForSeconds(blockDelay);
                 }
-                if (cityBlockType == 3)
+                if (cityBlockType2 == 3)
                 {
-                    Vector3 spawnPos = new Vector3(startPoint, 0f);
+                    Vector3 spawnPos = new Vector3(startPoint, 0f, -30f);
                     Instantiate(CityBlockPrefab3, spawnPos, Quaternion.identity);
                     yield return new WaitForSeconds(blockDelay);
                 }
